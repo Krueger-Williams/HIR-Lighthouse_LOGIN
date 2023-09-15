a49e0ab2ed972e94c522a04162a3c95145e48785
Krueger Williams <krueger.williams04@gmail.com>
2023-09-15 16:37:26 -0500

    User Sign In Complete

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
+import React from 'react';
+import { ThemeProvider } from 'react-native-elements';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import RootNavigation from './Navigation';
+
 export default function App() {
   return (
-    <View style={styles.container}>
+    /*<View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View>*/
+    <ThemeProvider>
+      <RootNavigation/>
+    </ThemeProvider>
+    
   );
 }
 
